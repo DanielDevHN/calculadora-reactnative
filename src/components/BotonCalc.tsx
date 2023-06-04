fd329270d1cca9045333bbbf625ac6bf914f501a
Daniel Gregorio Reyes Alvarez <daniel.reyes@grupocorinsa.hn>
2023-06-03 23:41:28 -0600

    Adding all buttons in component, styles and TouchableOpacity

--- src/components/BotonCalc.tsx
+++ src/components/BotonCalc.tsx
@@ -1,23 +1,33 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, TouchableOpacity, View } from 'react-native';
 import { styles } from '../theme/appTheme';
 
 interface Props {
     texto: string;
     color?: string;
+    ancho?: boolean;
 }
 
-export const BotonCalc = ( { texto, color}: Props) => {
+export const BotonCalc = ( { texto, color = '#2D2D2D', ancho = false}: Props) => {
   return (
     <View>
-        <View style={
-            {
-                ...styles.boton,
-                backgroundColor: color,
-            }
-        }>
-            <Text style={ styles.botonTexto }>{ texto }</Text>
-        </View>
+        <TouchableOpacity
+            onPress={ () => console.log(texto) }
+            activeOpacity={ 0.4 }
+        >
+            <View style={
+                {
+                    ...styles.boton,
+                    backgroundColor: color,
+                    width: (ancho) ? 180 : 80,
+                }
+            }>
+                <Text style={{
+                    ...styles.botonTexto,
+                    color: (color === '#9B9B9B') ? 'black' : 'white',
+                }}>{ texto }</Text>
+            </View>
+        </TouchableOpacity>
     </View>
   );
 };
