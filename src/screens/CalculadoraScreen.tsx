4fc0d15ecd503c1094699160cd7385d66ecc2582
Daniel Gregorio Reyes Alvarez <daniel.reyes@grupocorinsa.hn>
2023-06-03 21:57:12 -0600

    Add initial project in repo

---
+++ src/screens/CalculadoraScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const CalculadoraScreen = () => {
+  return (
+    <View>
+      <Text>Calculadora</Text>
+    </View>
+  );
+};
+
+export default CalculadoraScreen;
